bd2c0810e12134265f32c8f83be14208066177ff
Mynmhui <286734637@qq.com>
2022-08-05 14:44:36 +0800

    完成了组织架构布局

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,20 +1,57 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <TreeTools :isRoot="true" :treeNode="company"/>
+        <!-- 树形 -->
+        <el-tree
+        :data="treeData"
+        :props="defaultProps"
+        default-expand-all
+        >
+        <template v-slot="{ data }">
+          <TreeTools :treeNode="data"/>
+        </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
+import { getDeptsApi } from '@/api/departments'
+import { transListToTree } from '@/utils'
 export default {
-  data() {
-    return {}
+data() {
+return {
+    defaultProps: { label: 'name' },
+    company:{
+    name:'传智教育', manager:'负责人' 
+    },
+    treeData: [
+      { name: '总裁办', children: [{ name: '董事会' }] },
+    { name: '行政部' },
+      { name: '人事部' },
+    ],
+}
   },
 
-  created() {},
+  components: {
+    TreeTools
+  },
 
-  methods: {}
+  created() {
+  this.loadDepts()
+  },
+
+  methods: {
+  async loadDepts() {
+  const res = await getDeptsApi()
+  this.treeData = transListToTree(res.depts, '') 
+  }
+  }
 }
 </script>
 

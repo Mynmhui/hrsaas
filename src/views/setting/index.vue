29996e8944376e5d5e7dbd20f393e53e1da73eb3
Mynmhui <286734637@qq.com>
2022-08-11 11:48:35 +0800

    完成了角色管理新增功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -3,55 +3,140 @@
     <div class="app-container">
       <el-tabs v-model="activeName">
         <el-tab-pane label="角色管理" name="first">
-          <el-button type="primary">新增角色</el-button>
+          <el-button type="primary" @click="adddialogVisible = true"
+            >新增角色</el-button
+          >
           <!-- 表格 -->
-          <el-table :data="tableData" style="width: 100%">
-            <el-table-column prop="date" label="日期" width="180">
+          <el-table border :data="tableData" style="width: 100%">
+            <el-table-column type="index" label="序号" width="180">
             </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180">
+            <el-table-column prop="name" label="角色" width="180">
             </el-table-column>
-            <el-table-column prop="address" label="地址"> </el-table-column>
+            <el-table-column prop="description" label="描述" width="180">
+            </el-table-column>
+            <el-table-column prop="address" label="操作">
+              <el-button size="small" type="primary">分配权限</el-button>
+              <el-button size="small" type="success">编辑</el-button>
+              <el-button size="small" type="danger">删除</el-button>
+            </el-table-column>
           </el-table>
+          <el-pagination
+            layout="sizes, prev, pager, next"
+            :page-sizes="[3, 5, 10, 20]"
+            :total="total"
+            :page-size="pageSize"
+            @current-change="currentChange"
+            @size-change="handleSizeChange"
+          >
+          </el-pagination>
         </el-tab-pane>
-        <el-tab-pane label="公司信息" name="second">配置管理</el-tab-pane>
+        <el-tab-pane label="公司信息" name="second">
+          <el-alert
+            title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+            type="info"
+            show-icon
+            :closable="false"
+          />
+          <el-form label-width="120px" style="margin-top: 50px">
+            <el-form-item label="公司名称">
+              <el-input disabled style="width: 400px" />
+            </el-form-item>
+            <el-form-item label="公司地址">
+              <el-input disabled style="width: 400px" />
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <el-input disabled style="width: 400px" />
+            </el-form-item>
+            <el-form-item label="备注">
+              <el-input
+                type="textarea"
+                :rows="3"
+                disabled
+                style="width: 400px"
+              />
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
       </el-tabs>
     </div>
+    <!-- 添加角色对话框 -->
+    <el-dialog title="新增角色" :visible.sync="adddialogVisible" width="50%">
+      <el-form
+        ref="form"
+        :model="addRouleForm"
+        :rules="addRouleFormRules"
+        label-width="80px"
+      >
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="addRouleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input v-model="addRouleForm.region"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="onClonse">取 消</el-button>
+        <el-button type="primary" @click="onAddRule">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { getRolesApi, addRoleApi } from '@/api/role'
 export default {
   data() {
     return {
       activeName: 'first',
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄',
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄',
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄',
-        },
-      ],
+      tableData: [],
+      total: 0,
+      pageSize: 3,
+      page: 1,
+      adddialogVisible: false,
+      addRouleForm: {
+        name: '',
+        region: '',
+      },
+      addRouleFormRules: {
+        name: [{ required: true, message: '请输角色名称', trigget: 'blur' }],
+      },
     }
   },
 
-  created() {},
+  created() {
+    this.getRoles()
+  },
 
-  methods: {},
+  methods: {
+    async getRoles() {
+      const { rows, total } = await getRolesApi({
+        page: this.page,
+        pagesize: this.pageSize,
+      })
+      ;(this.tableData = rows), (this.total = total)
+    },
+    currentChange(val) {
+      this.page = val
+      this.getRoles()
+    },
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.getRoles()
+    },
+    onClonse() {
+      this.adddialogVisible = false
+    },
+    async onAddRule() {
+      await this.$refs.form.validate()
+      await addRoleApi(this.addRouleForm)
+      this.$message.success('添加成功')
+      this.adddialogVisible = false
+      this.getRoles()
+      this.addRouleForm.name = ''
+      this.addRouleForm.region = ''
+    },
+  },
+  dialogClose() {},
 }
 </script>
 
